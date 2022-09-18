a9388b75bcb3736705bf2797ef98f37f6243a595
hj20220908 <hj20220908@gmail.com>
2022-09-18 22:45:57 +0900

    커스텀 로그인 페이지 생성하기

---
+++ src/main/java/security/coreSpringSecurity/controller/login/LoginController.java
@@ -0,0 +1,17 @@
+package security.coreSpringSecurity.controller.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login/login";
+    }
+
+
+}
+
+
